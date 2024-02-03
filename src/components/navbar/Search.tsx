1d48053e801f558dfcaf6750b4a1db51962416f2
fjonamalo <fjonamalo0@gmail.com>
2024-02-03 19:02:48 +0100

    Refs to select DOM elements

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -1,9 +1,25 @@
+import { useRef, useEffect } from "react";
+
 interface SearchProps {
   query: string;
   setQuery: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const Search = ({ query, setQuery }: SearchProps) => {
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const callback = (e: { code: string }) => {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl.current?.focus();
+        setQuery("");
+      }
+    };
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <div>
       <input
@@ -12,6 +28,7 @@ const Search = ({ query, setQuery }: SearchProps) => {
         placeholder="Search movies..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        ref={inputEl}
       />
     </div>
   );
